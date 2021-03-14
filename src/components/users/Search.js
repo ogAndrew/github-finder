00d889ddbe2445de481f61b894664c6cef5e6f48
Andrew Talle <talle.andrew@gmail.com>
2021-03-13 17:23:46 -0700

    refactor app.js to hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -10,13 +10,13 @@ class Search extends Component {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
   };
 
   onSubmit = (e) => {
     e.preventDefault();
     if (this.state.text === '') {
-      this.props.setAlert('Please enter something', 'light');
+      this.props.showAlert('Please enter something', 'light');
     } else {
       this.props.searchUsers(this.state.text);
       this.setState({ text: '' });
